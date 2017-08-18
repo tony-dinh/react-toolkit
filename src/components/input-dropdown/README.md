dc6e2214a3fcd042b392210b7f8a3b95a96f5d11
peterwiebe <pjwiebe@gmail.com>
2017-08-18 11:18:15 -0700

    Add base styling to input-dropdown

---
+++ src/components/input-dropdown/README.md
@@ -0,0 +1,11 @@
+Example
+```
+const source = [
+    {value: 'pineapple', label: 'pineapple'},
+    {value: 'apple', label: 'apple'},
+    {value: 'pen', label: 'pen'}
+];
+
+
+   <InputDropdown source={source} />
+```
---
+++ src/components/input-dropdown/index.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+import Input from '../input'
+import Dropdown from '../dropdown'
+
+import './_base.scss'
+
+
+class InputDropdown extends React.PureComponent {
+    constructor(props) {
+        super(props)
+
+        this.onChange = this.onChange.bind(this)
+        
+        this.state = {
+            value: ''
+        }
+    }
+
+    onChange(selectedItem) {
+        if(selectedItem) {
+            this.setState({
+                value: selectedItem.value
+            })
+        } else {
+            this.setState({
+                value: ''
+            })
+        }
+    }
+
+    render() {
+
+        const {
+            className,
+            error,
+            source
+        } = this.props
+
+        const {
+            value
+        } = this.state
+
+        const classes = classNames('td-input-dropdown', className)
+
+        return (
+            <div className={classes}>
+                <Input 
+                    className="td-input-dropdown__selection"
+                    error={error}
+                    readOnly
+                    value={value}
+                />
+                <Dropdown 
+                    className="td-input-dropdown__list" 
+                    source={source}
+                    onItemSelected={this.onChange}
+                />
+            </div>
+        )
+    }
+}
+
+InputDropdown.PropTypes = {
+    className: PropTypes.string,
+    error: PropTypes.string,
+    source: PropTypes.array
+}
+
+export default InputDropdown
