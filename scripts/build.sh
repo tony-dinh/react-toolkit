ff511d095791b2990b8da30cb1710b68f7dec8e2
Tony Dinh <tony-dinh@users.noreply.github.com>
2017-10-26 11:46:25 -0700

    🔎 Expose `commonjs` Modules (#26)

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,11 +9,17 @@ rm -rf $ROOT_DIR/src/vendor
 mkdir -p $ROOT_DIR/src/vendor/styles/ && cp $ROOT_DIR/node_modules/flatpickr/dist/flatpickr.min.css $ROOT_DIR/src/vendor/styles/
 
 # Transpile Components
-./node_modules/.bin/babel src/components -x ".js",".jsx" --ignore "*.test.js","test.js" --out-dir dist/components
+./node_modules/.bin/babel src/components -x ".js",".jsx" --ignore "*.test.js","test.js" --out-dir dist/es6/components
+NODE_ENV='commonjs' ./node_modules/.bin/babel src/components -x ".js",".jsx" --ignore "*.test.js","test.js" --out-dir dist/commonjs/components
 
 # Copy Components styles
 pushd src
-rsync -Rv components/**/*.scss $ROOT_DIR/dist/
-rsync -Rv styles/*.scss $ROOT_DIR/dist/
-rsync -Rv vendor/**/*.* $ROOT_DIR/dist/
+rsync -Rv components/**/*.scss $ROOT_DIR/dist/es6
+rsync -Rv components/**/*.scss $ROOT_DIR/dist/commonjs
+
+rsync -Rv styles/*.scss $ROOT_DIR/dist/es6
+rsync -Rv styles/*.scss $ROOT_DIR/dist/commonjs
+
+rsync -Rv vendor/**/*.* $ROOT_DIR/dist/es6
+rsync -Rv vendor/**/*.* $ROOT_DIR/dist/commonjs
 popd
