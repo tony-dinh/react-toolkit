70a6fdd8d301ef06b53040062993ea80a45271db
tony-dinh <im.tonydinh@gmail.com>
2017-11-03 11:42:39 -0700

    ✨ add tab component logic

---
+++ src/components/tabs/index.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import Tab from './partials/tab'
+
+const noop = () => {}
+
+class Tabs extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            activeIndex: 0
+        }
+
+        this.onTabSelected = this.onTabSelected.bind(this)
+    }
+
+    onTabSelected(index, value) {
+        this.props.onTabSelected(index, value)
+
+        if (this.state.activeIndex !== index) {
+            this.setState({activeIndex: index}, () => { this.props.onTabChange(index, value) })
+        }
+    }
+
+    render() {
+        const {
+            children,
+            className,
+        } = this.props
+
+        const classes = classNames('td-tabs', className)
+        const TabElements = React.Children.map(children, (element, index) => (
+            React.cloneElement(element, {
+                active: index === this.state.activeIndex,
+                onSelect: () => {
+                    this.onTabSelected(index, element.props.value)
+                    element.props.onSelect(index, element.props.value)
+                }
+            })
+        ))
+
+        return (
+            <nav className={classes} role="tablist">
+                {TabElements}
+            </nav>
+        )
+    }
+}
+
+const TabType = (props, propName, componentName) => {
+    const allowedTypes = [Tab]
+
+    const children = React.Children.toArray(props[propName])
+
+    if (children.length === 0) {
+        return new Error(`The ${componentName} component requires at least one child element.`)
+    }
+
+    const isValid = children.every((child) => allowedTypes.includes(child.type))
+
+    if (isValid) {
+        return null
+    }
+
+    return new Error(`Invalid prop ${propName} supplied to ${componentName}.`)
+}
+
+Tabs.propTypes = {
+    children: TabType,
+    className: PropTypes.string,
+    onTabChange: PropTypes.func,
+    onTabSelected: PropTypes.func
+}
+
+Tabs.defaultProps = {
+    onTabChange: noop,
+    onTabSelected: noop
+}
+
+export {Tab}
+export default Tabs
